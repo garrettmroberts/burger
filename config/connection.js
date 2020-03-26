13872e073ae7fe17349093867dd2ef98105ca905
Garrett Roberts <garrettmroberts@gmail.com>
2020-03-26 09:25:16 -0500

    added orm object

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   port: process.env.PORT || 3306,
   database: "burgers_db",
   user: "root",
-  password: "BlueBird36!"
+  password: ""
 });
 
 connection.connect(err => {

74114bab1b5147aa4978259ac0c87bd57ad77d4d
Garrett Roberts <garrettmroberts@gmail.com>
2020-03-26 14:58:21 -0500

    created burger model

--- config/orm.js
+++ config/orm.js
@@ -1,22 +1,22 @@
 const connection = require("./connection");
 
 const orm = {
-  selectAll: function() {
-    connection.query("SELECT * FROM burgers", (err, res) => {
+  selectAll: function(table) {
+    connection.query("SELECT * FROM ??", [table], (err, res) => {
       if (err) throw err;
       return res;
     });
   },
 
-  insertOne: function(burger_name, devoured) {
-    const sql = connection.query("INSERT INTO burgers (burger_name, devoured) VALUES (?, ?)", [burger_name, devoured], (err, res) => {
+  insertOne: function(table, burger_name, devoured) {
+    const sql = connection.query("INSERT INTO ?? (burger_name, devoured) VALUES (?, ?)", [table, burger_name, devoured], (err, res) => {
       if (err) throw err;
       console.log(sql._results[0].affectedRows + " burger added.")
     });
   },
 
-  updateOne: function(id) {
-    const sql = connection.query("UPDATE burgers SET devoured = true WHERE id = ?", [id], (err, res) => {
+  updateOne: function(table, id) {
+    const sql = connection.query("UPDATE ?? SET devoured = true WHERE id = ?", [table, id], (err, res) => {
       if (err) throw err;
       console.log(sql._results[0].affectedRows + " burgers edited.")
     });
@@ -24,5 +24,3 @@ const orm = {
 };
 
 module.exports = orm;
-
-orm.selectAll();
--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,19 @@
 const orm = require("../config/orm");
+
+const burger = {
+  selectAll: function() {
+    orm.selectAll("burgers")
+  },
+
+  insertBurger: function(burger_name, devoured) {
+    orm.insertOne("burgers", burger_name, devoured);
+  },
+
+  updateBurger: function(id) {
+    orm.updateOne("burgers", id);
+  }
+}
+
+module.exports = burger;
+
+burger.updateBurger(3);

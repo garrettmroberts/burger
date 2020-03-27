0eaa44fed3010475939177260b361e4e6e6a5ea7
Garrett Roberts <garrettmroberts@gmail.com>
2020-03-27 17:30:28 -0500

    adjusted database port

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  port: process.env.PORT || 3306,
+  port: 3306,
   database: "burgers_db",
   user: "root",
   password: ""
